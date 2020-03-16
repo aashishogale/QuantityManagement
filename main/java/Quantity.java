5fc707eb7d458c4281c8ae9cc002046a8530978e
aashishogale <aashishogale9@gmail.com>
2020-03-16 12:14:01 +0530

    added compare uc

--- main/java/Quantity.java
+++ main/java/Quantity.java
@@ -5,11 +5,15 @@ public class Quantity {
 
     public HashMap<Quantity.unit, Double> quantity = new HashMap<>();
     enum unit {Inches, Yards, Foot};
-
-    public int value;
+    public double value;
 
     public Quantity(Quantity.unit unit, double value){
-        quantity.put(unit, value);
+
+        quantity.put(Quantity.unit.Yards, 36.0);
+        quantity.put(Quantity.unit.Foot, 12.0);
+        quantity.put(Quantity.unit.Inches, 1.0);
+        this.value = value*quantity.get(unit);
+
 
     }
 
@@ -19,7 +23,7 @@ public class Quantity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Quantity quantity1 = (Quantity) o;
-        return value == quantity1.value &&
+        return Double.compare(quantity1.value, value) == 0 &&
                 Objects.equals(quantity, quantity1.quantity);
     }
 
--- test/java/ConvertTest.java
+++ test/java/ConvertTest.java
@@ -4,8 +4,12 @@ import org.junit.Test;
 public class ConvertTest {
 
     @Test
-    public void convertToInches()  {
+    public void checkUnit()  {
         Assert.assertTrue(new Quantity(Quantity.unit.Inches, 0).equals(new Quantity(Quantity.unit.Inches,0)));
     }
 
+    @Test
+    public void checkForDifferentUnit()  {
+        Assert.assertTrue(new Quantity(Quantity.unit.Yards, 1).equals(new Quantity(Quantity.unit.Inches,36)));
+    }
 }
